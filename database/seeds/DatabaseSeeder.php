d4a42990675cb82fd1089df76e19b9376909bf8a
Gavin Dibley <gavin@advisercentre.com.au>
2020-04-11 23:11:47 +1000

    Add Thread and Reply models

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UserSeeder::class);
+        $threads = factory('App\Thread', 50)->create();
+
+        $threads->each(function ($thread) {
+            factory('App\Reply', 10)->create([
+                'thread_id' => $thread->id
+            ]);
+        });
     }
 }
