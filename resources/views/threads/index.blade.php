555003c550bf08cfe174410787c4696384252bfa
Gavin Dibley <gavin@gavindibley.id.au>
2020-05-07 09:17:19 +1000

    Add Create Thread button on top of topics list

--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -3,8 +3,10 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-6">
                 <h1 class="bd-title" id="content">Topics</h1>
+            </div><div class="col-md-2">
+                <a class="btn btn-primary float-right" href="{{ Route('threads.create') }}">Create Topic</a>
             </div>
         </div>
         <div class="row justify-content-center">
